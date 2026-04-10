e20e75c6e0bab741ddb0061a33801572d2e5b00c
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-10 18:28:20 +0200

    update constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF4A90E2);
+  static const Color primaryColor = Color(0xFF2E7D32);
+  static const Color secondaryColor = Color(0xFF4CAF50);
+  static const Color backgroundColor = Color(0xFFF5FBEF);
+  static const Color accentColor = Color(0xFF0288D1);
+  static const Color textColor = Color(0xFF263238);
 }
