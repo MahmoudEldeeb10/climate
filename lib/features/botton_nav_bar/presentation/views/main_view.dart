dbf4b3b016a470c081de571ac14cca2131b4c6f6
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-10 18:21:08 +0200

    crreate btn navbar

---
+++ lib/features/botton_nav_bar/presentation/views/main_view.dart
@@ -0,0 +1,84 @@
+import 'package:climate/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+import 'package:climate/features/chat/presentation/view/chat_view.dart';
+import 'package:climate/features/home/presentation/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MainView extends StatelessWidget {
+  MainView({super.key});
+
+  final List pages = [
+    HomeView(),
+    ChatView(),
+    HomeView(),
+    HomeView(),
+    HomeView(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BottomNavCubit, BottomNavState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: Stack(
+            children: [
+              pages[state.currentIndex],
+
+              Positioned(
+                bottom: 16,
+                left: 16,
+                right: 16,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black26, blurRadius: 10),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: BottomNavigationBar(
+                      backgroundColor: Colors.black,
+                      currentIndex: state.currentIndex,
+                      onTap: context.read<BottomNavCubit>().changeTab,
+
+                      showSelectedLabels: false,
+                      showUnselectedLabels: false,
+                      type: BottomNavigationBarType.fixed,
+
+                      selectedItemColor: Colors.white,
+                      unselectedItemColor: Colors.grey,
+
+                      items: const [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home_outlined),
+                          label: '',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.chat_outlined),
+                          label: '',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: '',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: '',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: '',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:climate/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+import 'package:climate/features/botton_nav_bar/presentation/views/main_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const ClimateApp());
@@ -9,6 +12,21 @@ class ClimateApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return BlocProvider(
+      create: (context) => BottomNavCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            // elevation: 0,
+          ),
+        ),
+        home: MainView(),
+      ),
+    );
   }
 }
