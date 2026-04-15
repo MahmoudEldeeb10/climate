914acc2a1076adf9284b39b54859494f0397cfbc
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-15 20:17:20 +0200

    create custom search file

--- lib/features/home/presentation/home_view.dart
+++ lib/features/home/presentation/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:climate/constants.dart';
+import 'package:climate/features/home/presentation/view/widgets/custom_search.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -11,32 +12,6 @@ class HomeView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [SizedBox(height: 50), CustomSearch()],
-        ),
-      ),
-    );
-  }
-}
-
-class CustomSearch extends StatelessWidget {
-  const CustomSearch({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 22.0),
-      child: TextField(
-        style: const TextStyle(color: AppColors.primaryText),
-        cursorColor: AppColors.primaryText,
-        decoration: InputDecoration(
-          hintText: 'Search for a city',
-          hintStyle: TextStyle(color: AppColors.primaryText),
-          filled: true,
-          fillColor: AppColors.cardBackgroundColor,
-          prefixIcon: const Icon(Icons.search, color: AppColors.primaryText),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
         ),
       ),
     );
