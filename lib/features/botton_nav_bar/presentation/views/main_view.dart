7486838f4d6a41b33a07130e9d74343af90903fa
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-15 19:17:33 +0200

    edit btn nav bar

--- lib/features/botton_nav_bar/presentation/views/main_view.dart
+++ lib/features/botton_nav_bar/presentation/views/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:climate/constants.dart';
+import 'package:climate/features/ai_insights/presentation/view/ai_insights_view.dart';
 import 'package:climate/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
 import 'package:climate/features/chat/presentation/view/chat_view.dart';
 import 'package:climate/features/home/presentation/home_view.dart';
@@ -8,13 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MainView extends StatelessWidget {
   MainView({super.key});
 
-  final List pages = [
-    HomeView(),
-    ChatView(),
-    HomeView(),
-    HomeView(),
-    HomeView(),
-  ];
+  final List pages = [HomeView(), AIInsightsView(), ChatView()];
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BottomNavCubit, BottomNavState>(
@@ -27,52 +22,36 @@ class MainView extends StatelessWidget {
 
               Positioned(
                 bottom: 16,
-                left: 16,
-                right: 16,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(color: Colors.black26, blurRadius: 10),
+                left: 52,
+                right: 52,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: BottomNavigationBar(
+                    backgroundColor: AppColors.cardBackgroundColor,
+                    currentIndex: state.currentIndex,
+                    onTap: context.read<BottomNavCubit>().changeTab,
+
+                    showSelectedLabels: true,
+                    showUnselectedLabels: false,
+                    type: BottomNavigationBarType.fixed,
+
+                    selectedItemColor: AppColors.primaryText,
+                    unselectedItemColor: AppColors.primaryText,
+
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home_outlined),
+                        label: 'Home',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.insights_outlined),
+                        label: 'Insights',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.chat_outlined),
+                        label: 'Chat',
+                      ),
                     ],
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30),
-                    child: BottomNavigationBar(
-                      backgroundColor: AppColors.backgroundColor,
-                      currentIndex: state.currentIndex,
-                      onTap: context.read<BottomNavCubit>().changeTab,
-
-                      showSelectedLabels: false,
-                      showUnselectedLabels: false,
-                      type: BottomNavigationBarType.fixed,
-
-                      selectedItemColor: AppColors.primaryColor,
-                      unselectedItemColor: AppColors.textColor,
-
-                      items: const [
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.home_outlined),
-                          label: '',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.chat_outlined),
-                          label: '',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.home),
-                          label: '',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.home),
-                          label: '',
-                        ),
-                        BottomNavigationBarItem(
-                          icon: Icon(Icons.home),
-                          label: '',
-                        ),
-                      ],
-                    ),
                   ),
                 ),
               ),
