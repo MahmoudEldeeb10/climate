9478e9afde6a6ac21d8b3e6a0046d8c0c02fe415
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-15 18:49:50 +0200

    create ai insighrs view

---
+++ lib/features/ai_insights/presentation/view/ai_insights_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AIInsightsView extends StatelessWidget {
+  const AIInsightsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
