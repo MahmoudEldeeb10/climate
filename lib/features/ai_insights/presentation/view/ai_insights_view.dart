77c3213753a441cc87b4ad0104d50be4877766a6
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-15 19:49:07 +0200

    add safe area to all project

--- lib/features/ai_insights/presentation/view/ai_insights_view.dart
+++ lib/features/ai_insights/presentation/view/ai_insights_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AIInsightsView extends StatelessWidget {
   const AIInsightsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Center(child: Text('AI Insights Coming Soon!')),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/chat/presentation/view/chat_view.dart
+++ lib/features/chat/presentation/view/chat_view.dart
@@ -5,6 +5,15 @@ class ChatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Center(child: Text('Chat Bot Coming Soon!')),
+          ],
+        ),
+      ),
+    );
   }
 }
