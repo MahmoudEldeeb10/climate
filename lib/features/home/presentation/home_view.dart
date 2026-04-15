85a3f3ac6fd3556bf888a021f7e01505c49d07df
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-15 19:46:47 +0200

    create search widget ui

--- lib/features/home/presentation/home_view.dart
+++ lib/features/home/presentation/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:climate/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,8 +6,39 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [SizedBox(height: 50), CustomSearch()],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSearch extends StatelessWidget {
+  const CustomSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22.0),
+      child: TextField(
+        style: const TextStyle(color: AppColors.primaryText),
+        cursorColor: AppColors.primaryText,
+        decoration: InputDecoration(
+          hintText: 'Search for a city',
+          hintStyle: TextStyle(color: AppColors.primaryText),
+          filled: true,
+          fillColor: AppColors.cardBackgroundColor,
+          prefixIcon: const Icon(Icons.search, color: AppColors.primaryText),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
     );
   }
 }
