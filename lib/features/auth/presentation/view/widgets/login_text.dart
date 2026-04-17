b47769d0ebaec5d5ce83b2dc27235df439d66957
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-17 13:40:10 +0200

    create login text to navigae to login view

---
+++ lib/features/auth/presentation/view/widgets/login_text.dart
@@ -0,0 +1,27 @@
+import 'package:climate/appnavigator.dart';
+import 'package:climate/core/utils/styles.dart';
+import 'package:climate/features/auth/presentation/view/login_view.dart';
+import 'package:flutter/material.dart';
+
+class LoginText extends StatelessWidget {
+  const LoginText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('Already have an account? ', style: Styles.textStyle14),
+        GestureDetector(
+          onTap: () {
+            AppNavigator.goToAndClearStack(context, LoginView());
+          },
+          child: Text(
+            'Sign Up',
+            style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
