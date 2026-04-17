bdb9a2fbc8a1452f49e2494ace25dbd7a3f0529f
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-17 11:27:52 +0200

    create tex styles folder

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,18 @@
+import 'package:climate/constants.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const textStyle30 = TextStyle(
+    color: AppColors.primaryText,
+    fontSize: 30,
+    // fontWeight: FontWeight.bold,
+  );
+  static const textStyle14 = TextStyle(
+    color: AppColors.primaryText,
+    fontSize: 14,
+  );
+  static const textStyle16 = TextStyle(
+    color: AppColors.primaryText,
+    fontSize: 16,
+  );
+}
