29810e2b342af690e915415ac762b6a545c141d5
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-17 11:28:20 +0200

    utton

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final Color textColor;
+  final Color? sideColor;
+  final Icon? icon;
+
+  void Function() onpressed;
+  CustomButton({
+    super.key,
+    required this.text,
+    required this.onpressed,
+    required this.color,
+    required this.textColor,
+    this.icon,
+    this.sideColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: ElevatedButton(
+        onPressed: onpressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+            side: BorderSide(color: sideColor ?? Colors.transparent),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null) ...[
+              Icon(icon!.icon, color: Colors.black, size: 24),
+              const SizedBox(width: 8),
+            ],
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: textColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
