25f839ee7a0ba10fca8de9b825a94107835bb4f6
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-17 11:28:12 +0200

    add color to constant colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 class AppColors {
   static const Color primaryText = Color(0xFFF9F9F9);
   static const Color secondaryText = Color(0xFFB1CCF9);
+  static const Color tertiaryText = Color(0xFF155DFC);
   static const Color backgroundColor = Color(0xFF2576F9);
   static const Color cardBackgroundColor = Color(0xFF6D98ED);
   static const Color cardBackgroundColor2 = Color(0xFF3177F9);
