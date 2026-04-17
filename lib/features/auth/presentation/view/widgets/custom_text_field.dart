453cf4aa9362d0c084ae59d422f4a1a1391af082
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-17 11:29:39 +0200

    create custom text field

---
+++ lib/features/auth/presentation/view/widgets/custom_text_field.dart
@@ -0,0 +1,87 @@
+import 'package:climate/constants.dart';
+import 'package:climate/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String hint;
+  final TextInputType keyboardType;
+  final bool isPassword;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hint,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.validator,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: const TextStyle(color: AppColors.primaryText),
+      cursorColor: AppColors.primaryText,
+
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      obscureText: _obscureText,
+      validator: widget.validator,
+      decoration: InputDecoration(
+        hintText: widget.hint,
+        hintStyle: Styles.textStyle14.copyWith(
+          color: AppColors.primaryText.withOpacity(0.7),
+        ),
+        prefixIcon: widget.prefixIcon,
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : widget.suffixIcon,
+        suffixIconColor: AppColors.primaryText.withOpacity(0.7),
+        // border styles
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.primaryText, width: 0.5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.primaryText, width: 0.5),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.primaryText, width: 0.5),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.primaryText, width: 0.5),
+        ),
+      ),
+    );
+  }
+}
