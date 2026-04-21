13727592b872c6636f3937786ef9a3cd502af652
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-04-21 22:49:19 +0200

    create current weather widget

---
+++ lib/features/home/presentation/view/widgets/current_weather_section.dart
@@ -0,0 +1,35 @@
+import 'package:climate/constants.dart';
+import 'package:climate/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class CurrentWeatherSection extends StatelessWidget {
+  const CurrentWeatherSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(Icons.location_on_outlined, color: AppColors.primaryText),
+            SizedBox(width: 5),
+            Text(
+              "San Francisco",
+              style: TextStyle(color: AppColors.primaryText, fontSize: 20),
+            ),
+          ],
+        ),
+
+        Icon(Icons.cloud, size: 80, color: AppColors.primaryText),
+        SizedBox(height: 10),
+        Text("72°", style: Styles.textStyle30.copyWith(fontSize: 60)),
+        Text(
+          "Partly Cloudy",
+          style: TextStyle(fontSize: 18, color: Colors.white70),
+        ),
+      ],
+    );
+  }
+}
