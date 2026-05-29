3e74fcce4ebebccc293cf381e989025e6a3be157
Mahmoud Elsayed Eldeeb <https://github.com/MahmoudEldeeb10>
2026-05-29 11:27:01 +0300

    fix weather cubit

--- lib/features/home/presentation/manager/weather_cubit.dart
+++ lib/features/home/presentation/manager/weather_cubit.dart
@@ -6,13 +6,15 @@ class WeatherCubit extends Cubit<WeatherState> {
   WeatherCubit() : super(WeatherInitial());
 
   final WeatherService _service = WeatherService();
-
   Future<void> getWeather() async {
+    if (isClosed) return;
     emit(WeatherLoading());
     try {
       final weather = await _service.fetchWeather();
+      if (isClosed) return;
       emit(WeatherSuccess(weather));
     } catch (e) {
+      if (isClosed) return;
       emit(WeatherFailure(e.toString()));
     }
   }
