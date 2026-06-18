0b113e1aaa7b83b268fd165f03df2dc5cdcb558a
hosametm <hosammetman@gmail.com>
2026-06-18 23:25:31 +0300

    Revert "feat: implement Firebase authentication system with registration, login, and password reset flows"
    This reverts commit c7f419287dd6250d6f9c5314346d8e1b7ba21946.

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,56 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart';
-import 'routes/app_routes.dart';
-import 'screens/home_screen.dart';
-import 'screens/login_screen.dart';
+  import 'package:climate/constants.dart';
+  import 'package:climate/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+  import 'package:climate/features/chat/presentation/view_model/cubit/chat_cubit.dart';
+  import 'package:climate/features/splash/presentation/view/splash_view.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyC0Y4rVWac59uRuAUdeOKt8fyndKTFnL5w",
-      authDomain: "auth-69d2c.firebaseapp.com",
-      projectId: "auth-69d2c",
-      storageBucket: "auth-69d2c.firebasestorage.app",
-      messagingSenderId: "1083823535407",
-      appId: "1:1083823535407:web:cc3924cbbd22e60efa1dda",
-      measurementId: "G-3075D2TBD6",
-    ),
-  );
-  
-  runApp(const MyApp());
-}
+  void main() {
+    runApp(const ClimateApp());
+  }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  class ClimateApp extends StatelessWidget {
+    const ClimateApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Auth',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: Colors.grey[100],
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: BorderSide.none,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-              borderSide: const BorderSide(color: Colors.blue, width: 2),
-            ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              foregroundColor: Colors.white,
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-          ),
+    @override
+    Widget build(BuildContext context) {
+      return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => BottomNavCubit()),
+          BlocProvider(create: (context) => ChatCubit()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(scaffoldBackgroundColor: AppColors.backgroundColor),
+          home: const SplashView(),
         ),
-        home: const AuthWrapper(),
-        routes: AppRoutes.routes,
-      ),
-    );
+      );
+    }
   }
-}
 
-class AuthWrapper extends StatelessWidget {
-  const AuthWrapper({Key? key}) : super(key: key);
+// import 'package:climate/constants.dart';
+// import 'package:climate/features/botton_nav_bar/presentation/manager/cubit/bottom_nav_cubit.dart';
+// import 'package:climate/features/splash/presentation/view/splash_view.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Provider.of<AuthProvider>(context, listen: false).authStateChanges,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-        if (snapshot.hasData) {
-          return const HomeScreen();
-        }
-        return const LoginScreen();
-      },
-    );
-  }
-}
+// void main() {
+//   runApp(const ClimateApp());
+// }
+
+// class ClimateApp extends StatelessWidget {
+//   const ClimateApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocProvider(
+//       create: (context) => BottomNavCubit(),
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+
+//         theme: ThemeData(scaffoldBackgroundColor: AppColors.backgroundColor),
+//         home: const SplashView(),
+//       ),
+//     );
+//   }
+// }
